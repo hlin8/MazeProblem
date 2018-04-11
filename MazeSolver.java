56685413750e5c21df8619947f79c3f60a5c2e91
Michelle Yang <zyang2@stuy.edu>
2018-04-11 18:46:41 -0400

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,3 +1,11 @@
 /** 
   Determines whether there exists a path through a maze starting at a designated beginning and ending at the treasure
   **/ 
+public class MazeSolver{
+  public boolean solveMaze(Maze m){
+    if (explorerIsOnA() == 0) 
+      return true;
+    else if (explorerIsOnA() == 1) 
+      return false; 
+  } 
+} 
