3444db729b734914dbd72a2485eb40a13b9638f2
Michelle Yang <zyang2@stuy.edu>
2018-04-11 18:48:47 -0400

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -3,9 +3,5 @@
   **/ 
 public class MazeSolver{
   public boolean solveMaze(Maze m){
-    if (explorerIsOnA() == 0) 
-      return true;
-    else if (explorerIsOnA() == 1) 
-      return false; 
   } 
 } 
